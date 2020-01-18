d46dc98f964604da07c5af2c5431e54a65c3abc4
sanjeetma <sanjeetma@outlook.com>
2020-01-18 17:08:19 +0530

    maven

--- fundooo/src/main/java/com/bridgelabz/fundo/Fundooo.java
+++ fundooo/src/main/java/com/bridgelabz/fundo/Fundooo.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FunAndDoApplication {
+public class Fundooo {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FunAndDoApplication.class, args);
+		SpringApplication.run(Fundooo.class, args);
 	}
 
 }
