36ee98c387942af8c2d42a603c5f13391d0ce990
Sanjeet Kumar <sanjeetma@outlook.com>
2020-01-25 13:56:20 +0530

    Update FunAndDoApplicationTests.java

--- fundooo/src/test/java/com/bridgelabz/springboot/FunAndDoApplicationTests.java
+++ fundooo/src/test/java/com/bridgelabz/springboot/FunAndDoApplicationTests.java
@@ -1,4 +1,4 @@
-/*package com.bridgelabz.springboot;
+package com.bridgelabz.springboot;
 
 import static org.junit.Assert.assertEquals;
 
@@ -63,4 +63,4 @@ public class FunAndDoApplicationTests {
 	}
 
 }
-*/
+
